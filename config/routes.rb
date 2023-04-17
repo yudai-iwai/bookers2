327f3bdb1652323f5a292d5a4e9577b0e6fcd926
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-04-17 06:56:00 +0000

    [3]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to:"homes#top"
+  root to: 'homes#top'
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
   resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
