ae8ec3f52f1f5b413fa6164bcd57d9281e2cb217
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-04-19 05:34:31 +0000

    [6]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
 
-before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
-    user_path
+    user_path(current_user.id)
   end
   
   def after_sign_out_path_for(resource)
@@ -13,7 +13,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
-　def index
-　end
-  
   def show
     @user = User.find(params[:id])
     @books = @user.books
   end
 
-  def 
+  def edit
     @user = User.find(params[:id])
   end
 end
