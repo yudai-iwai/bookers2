ddd81ab1a00ebb8912721d4d25a71e4173b81df7
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-04-19 08:12:37 +0000

    [7]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
   end
 
   def index
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
